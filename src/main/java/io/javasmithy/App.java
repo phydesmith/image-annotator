9c05c446958876624fe8f8b8f5b1245183cc6a69
Peter <me@me>
2021-03-09 15:27:06 -0600

    first commit

---
+++ src/main/java/io/javasmithy/App.java
@@ -0,0 +1,33 @@
+
+package io.javasmithy;
+
+import io.javasmithy.controller.MainController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main-layout.fxml"));
+            stage.setScene(new Scene( (Parent) loader.load(), 800, 400));
+            ((MainController)loader.getController()).setStage(stage);
+            stage.setTitle("Image Annotator");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/io/javasmithy/controller/MainController.java
@@ -0,0 +1,21 @@
+package io.javasmithy.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    private Stage stage;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+
+    public void setStage(Stage stage){
+        this.stage = stage;
+    }
+
+}
